db871d048945c14af321b24e66d5aa142db33d7c
Kirk J-Son Matic <kirkmatic1@gmail.com>
2024-06-26 19:45:15 +0800

    Update HomePage.java

--- src/HomePage.java
+++ src/HomePage.java
@@ -1,59 +1,43 @@
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import java.awt.Color;
-import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.SwingConstants;
+import java.awt.*;
+import javax.swing.*;
 
 public class HomePage extends JFrame {
 
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+    private static final long serialVersionUID = 1L;
+    private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					HomePage frame = new HomePage();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            try {
+                HomePage frame = new HomePage();
+                frame.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
 
-	/**
-	 * Create the frame.
-	 */
-	public HomePage() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1020, 721);
-		contentPane = new JPanel();
-		setLocationRelativeTo(null);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+    public HomePage() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 1020, 721);
+        setLocationRelativeTo(null);
 
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JPanel panel = new JPanel();
-		panel.setBackground(new Color(51, 0, 102));
-		panel.setBounds(0, 0, 1000, 689);
-		contentPane.add(panel);
-		panel.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("Welcome Nig--s");
-		lblNewLabel.setForeground(new Color(0, 255, 64));
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("Euphoria Party", Font.PLAIN, 60));
-		lblNewLabel.setBounds(200, 243, 599, 202);
-		panel.add(lblNewLabel);
-	}
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
 
+        JPanel panel = new JPanel();
+        panel.setBackground(new Color(51, 0, 102));
+        panel.setBounds(0, 0, 1000, 689);
+        contentPane.add(panel);
+        panel.setLayout(null);
+
+        JLabel lblNewLabel = new JLabel("Welcome User");
+        lblNewLabel.setForeground(new Color(0, 255, 64));
+        lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        lblNewLabel.setFont(new Font("Euphoria Party", Font.PLAIN, 60));
+        lblNewLabel.setBounds(200, 243, 599, 202);
+        panel.add(lblNewLabel);
+    }
 }
